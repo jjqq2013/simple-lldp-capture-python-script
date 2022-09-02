852cb91a2f0ced2f0eb6bf25a542ddd41a68921f
jjqq <5234060+jjqq2013@users.noreply.github.com>
2022-09-02 15:03:12 +0900

    Update lldp.sh

--- lldp.sh
+++ lldp.sh
@@ -25,7 +25,12 @@ set -e -o pipefail # exit on error
 # I could use advanced bash features to completely make this script not using any file, but
 # since this script may be called in some lower version of bash, I did not do not do that.
 
-function cleanup() { rm -fR /tmp/lldp-$$-*; }
+function cleanup() {
+  while read nic_name; do
+    ip link set dev $nic_name allmulticast off || true
+  done < /tmp/lldp-$$-CONNECTED_PHYSICAL_NICS
+  rm -fR /tmp/lldp-$$-*
+}
 trap cleanup INT
 
 find /sys/class/net -mindepth 1 -maxdepth 1 -type l -not -lname '*/virtual/*' -printf '%f\n' > /tmp/lldp-$$-PHYSICAL_NICS
@@ -33,31 +38,29 @@ ip -oneline link show up | grep ,LOWER_UP | awk '{print $2}' | tr -d ':' > /tmp/
 fgrep --line-regexp -f /tmp/lldp-$$-PHYSICAL_NICS /tmp/lldp-$$-CONNECTED_NICS > /tmp/lldp-$$-CONNECTED_PHYSICAL_NICS
 
 while read nic_name; do
-  ip link set dev $nic_name allmulticast on
+  # On some OSes, need enable allmulticast otherwise can not receive LLDP packets.
+  # Enabling promisc also works, but the nic may originally is already in promisc mode, I can not simply restore it safely at the end.
+  ip link set dev $nic_name allmulticast on || true
+
   mac=$(ip -oneline link show $nic_name | grep -Po '(?<= link/ether )([0-9a-f]{2}:){5}[0-9a-f]{2}')
 
   # run a background job to capture lldp (within 60s), save to $nic_name.xml
   rm -f /tmp/lldp-$$-$nic_name.xml
   sed 's/^    //g' <<'__EOF_OF_PYTHON_SCRIPT' | timeout 60 "$(type -p "${PYTHON:-python3}" || echo python)" - $nic_name $mac > /tmp/lldp-$$-$nic_name.xml &
     import socket, sys, re
-
     def memview_to_mac(data):
       return ':'.join('{:02x}'.format(x) for x in data.tolist())
-
     def to_int(char_or_int):
       if type(char_or_int) is int:
         return char_or_int
       return ord(char_or_int)
-
     def memview_to_str(data):
       return data.tobytes().decode('utf-8', errors='ignore')
-
     ETH_P_ALL = 3
     my_nic = sys.argv[1]
     my_mac = sys.argv[2]
     sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
     sock.bind((my_nic, 0))
-
     while True:
       data = memoryview(sock.recv(1514))
       dst_mac = memview_to_mac(data[:6])
@@ -70,18 +73,15 @@ while read nic_name; do
       if proto != b'\x88\xcc':
         continue
       data = data[14:]
-
       chassis_mac = ''
       port_id = ''
       sys_name = ''
-
       while True:
         tlv_type = to_int(data[0]) >> 1
         if tlv_type == 0:
             break
         tlv_len = ((to_int(data[0]) & 1) << 8) + to_int(data[1])
         tlv_data = data[2:2+tlv_len]
-
         # about the tlv_type, see LLDP_TLV_.* in lldp-tlv.h, about the tlv_data[0], it is a subtype, see LLDP_.*_SUBTYPE_.* in lldp-tlv.h
         if tlv_type == 1 and to_int(tlv_data[0]) == 4:  # LLDP_TLV_CHASSIS_ID=1, LLDP_CHASSISID_SUBTYPE_LLADDR=4
           chassis_mac = memview_to_mac(tlv_data[1:])
@@ -93,9 +93,7 @@ while read nic_name; do
             port_id = memview_to_str(tlv_data[1:])
         elif tlv_type == 5:  # LLDP_TLV_SYSTEM_NAME=5
           sys_name = memview_to_str(tlv_data)
-
         data = data[2+tlv_len:]
-
       if chassis_mac and port_id and sys_name and chassis_mac != my_mac:
         print('  <interface label="Interface" name="' + my_nic + '">')
         print('    <id label="ChassisID" type="mac">' + chassis_mac + '</id>')
